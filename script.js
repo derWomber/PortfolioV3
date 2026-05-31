355e19d78d27eabbe1f30eae67466268c56259cc
derWomber <youssef.edu.2023@gmail.com>
2026-05-31 20:07:17 +0100

    added AudioIntro

--- script.js
+++ script.js
@@ -9,11 +9,25 @@ function hideSidebar() {
 
 let Width = window.innerWidth;
 if (Width < 801) {
-    document.getElementById("pOn").textContent = "Entwickler, HTML/CSS/JS/Python, sucht Ausbildung als Fachinformatiker Anwendungsentwicklung."
+  document.getElementById("pOn").textContent =
+    "Entwickler, HTML/CSS/JS/Python, sucht Ausbildung als Fachinformatiker Anwendungsentwicklung.";
 }
 
+const audioPlayer = document.getElementById("AudioPlayer");
+const audio = new Audio("assets/audio.mp3");
 
-
+audioPlayer.addEventListener("click", () => {
+  if (audio.currentTime === 0) {
+    audio.play();
+    audioPlayer.style.fill = "#fff";
+    audioPlayer.style.scale = 1.2;
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+    audioPlayer.style.fill = "var(--Accent)";
+    audioPlayer.style.scale = 1;
+  }
+});
 
 const translations = {
   de: {
@@ -22,66 +36,69 @@ const translations = {
     education: "<span>Bildung</span>",
     role: "Fachinformatiker für Anwendungsentwicklung",
     on: "Offen für neue Chancen · Umzugsbereit",
-    pOn_mobile: "Entwickler, HTML/CSS/JS/Python, sucht Ausbildung als Fachinformatiker Anwendungsentwicklung.",
-    pOn:"Entwickler mit praktischer Erfahrung in HTML, CSS, JavaScript und Python. Abgeschlossene Webentwicklungsausbildung, PKW Führerschein",
+    pOn_mobile:
+      "Entwickler, HTML/CSS/JS/Python, sucht Ausbildung als Fachinformatiker Anwendungsentwicklung.",
+    pOn: "Entwickler mit praktischer Erfahrung in HTML, CSS, JavaScript und Python. Abgeschlossene Webentwicklungsausbildung, PKW Führerschein",
     cv: "⬇ Lebenslauf",
-    contact:"Kontact"
-    
+    contact: "Kontact",
   },
   en: {
     home: "<span>Home</span>",
     projects: "<span>Projects</span>",
     education: "<span>Education</span>",
-    role:"IT specialist for application development",
+    role: "IT specialist for application development",
     on: "Open to new opportunities · Ready to relocate",
-    pOn_mobile: "Developer, HTML/CSS/JS/Python, seeks apprenticeship as an IT specialist in application development.",
-    pOn:"Developer with practical experience in HTML, CSS, JavaScript, and Python. Completed web development training, driver's license (Class B).",
+    pOn_mobile:
+      "Developer, HTML/CSS/JS/Python, seeks apprenticeship as an IT specialist in application development.",
+    pOn: "Developer with practical experience in HTML, CSS, JavaScript, and Python. Completed web development training, driver's license (Class B).",
     cv: "⬇ Download CV",
-    contact:"Contact",
+    contact: "Contact",
   },
   fr: {
     home: "<span>Accueil</span>",
     projects: "<span>Projets</span>",
     education: "<span>Éducation</span>",
-    role:"Spécialiste informatique en développement d'applications",
+    role: "Spécialiste informatique en développement d'applications",
     on: "Ouvert aux nouvelles opportunités · Prêt à déménager",
-    pOn_mobile:"Développeur HTML/CSS/JS/Python, recherche un apprentissage en tant que spécialiste informatique en développement d'applications.",
-    pOn:"Développeur possédant une expérience pratique en HTML, CSS, JavaScript et Python. Formation en développement web complétée. Permis de conduire (catégorie B).",
+    pOn_mobile:
+      "Développeur HTML/CSS/JS/Python, recherche un apprentissage en tant que spécialiste informatique en développement d'applications.",
+    pOn: "Développeur possédant une expérience pratique en HTML, CSS, JavaScript et Python. Formation en développement web complétée. Permis de conduire (catégorie B).",
     cv: "⬇ Télécharger CV",
-    contact:"Me contacter",
+    contact: "Me contacter",
   },
 };
 
 function switchLanguage(lang) {
-  hideSidebar()
-  
-  document.getElementById("home").innerHTML = translations[lang].home
-  
-  document.getElementById("projects").innerHTML = translations[lang].projects
-  
-  document.getElementById("education").innerHTML = translations[lang].education
-    
-  document.getElementById("home_sidebar").textHTML = translations[lang].home
-  
-  document.getElementById("projects_sidebar").textHTML = translations[lang].projects
-  
-  document.getElementById("education_sidebar").textHTML = translations[lang].education
-  
-  document.getElementById("role").textContent = translations[lang].role
-  
-  document.getElementById("on").textContent = translations[lang].on
+  hideSidebar();
 
-  document.getElementById("lebenslaufButton").textContent = translations[lang].cv
-  document.getElementById("kontactButton").textContent = translations[lang].contact
+  document.getElementById("home").innerHTML = translations[lang].home;
+
+  document.getElementById("projects").innerHTML = translations[lang].projects;
+
+  document.getElementById("education").innerHTML = translations[lang].education;
+
+  document.getElementById("home_sidebar").textHTML = translations[lang].home;
+
+  document.getElementById("projects_sidebar").textHTML =
+    translations[lang].projects;
+
+  document.getElementById("education_sidebar").textHTML =
+    translations[lang].education;
+
+  document.getElementById("role").textContent = translations[lang].role;
+
+  document.getElementById("on").textContent = translations[lang].on;
+
+  document.getElementById("lebenslaufButton").textContent =
+    translations[lang].cv;
+  document.getElementById("kontactButton").textContent =
+    translations[lang].contact;
 
   let Width = window.innerWidth;
 
-if (Width < 801) {
-    document.getElementById("pOn").textContent = translations[lang].pOn_mobile
-}
-else{
-    document.getElementById("pOn").textContent = translations[lang].pOn
-}
-
-
+  if (Width < 801) {
+    document.getElementById("pOn").textContent = translations[lang].pOn_mobile;
+  } else {
+    document.getElementById("pOn").textContent = translations[lang].pOn;
+  }
 }
